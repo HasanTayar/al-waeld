64d0c6a2ce11e6e5678d3b642273a259a7c71ffd
Hasan <hasantayar1602@gmail.com>
2024-01-02 19:17:55 +0200

    trying to force the theme to be always on light mode

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import { queryClient } from '@/lib/query/queryClient';
 import { store } from './store/store.ts';
 import {Provider} from 'react-redux'
 import AppRoutes from './routes.tsx';
+const root = window.document.documentElement
+root.classList.add('light')
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
